7d285e8107da053a5b3cfc89447f565d088988aa
Cervoise <antoine.cervoise@gmail.com>
2018-11-04 23:21:41 +0100

    Update my-game-is-a-keyboard.ino
    Switch to english version of Hello World.

--- boards/gamebuino/my-game-is-a-keyboard/my-game-is-a-keyboard.ino
+++ boards/gamebuino/my-game-is-a-keyboard/my-game-is-a-keyboard.ino
@@ -1,5 +1,9 @@
+//Original code from: https://gamebuino.com/creations/hello-world
+
 #include <Gamebuino-Meta.h>
+
 #include "Keyboard.h"
+
 void setup() {
   gb.begin();
   Keyboard.begin();
@@ -8,14 +12,8 @@ void setup() {
 void loop() {
   while(!gb.update());
   gb.display.clear();
-   Keyboard.print("You can send a payload!");
-  //Affichage du message à l'écran
-  gb.display.print("'hello, world'\nest une tradition\ninstauree en\n\n");
-  //Texte à l'échelle 2
-  gb.display.setFontSize(2);
-  gb.display.print("1978");
-  //Retour à l'échelle 1 et changement de couleur
-  gb.display.setFontSize(1);
-  gb.display.setColor(BROWN);
-  gb.display.print("\n\n\n       - GAMEBUINO");
+  Keyboard.print("You can send a payload!");
+
+  // This is where most of the program takes place
+  gb.display.print("hello, world");
 }
